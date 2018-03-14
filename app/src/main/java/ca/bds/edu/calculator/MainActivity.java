9543050af4ebdc4b2f2b17d67d9349a2e7cb6d88
Derek Blue <derekmblue4011@gmail.com>
2018-03-14 20:09:48 -0300

    Top Bar commencement and refactorings
    Started implementing top nav bar buttons, finished seleted highlighter. Refactored some code to avoid hard-coded strings

--- app/src/main/java/ca/bds/edu/calculator/MainActivity.java
+++ app/src/main/java/ca/bds/edu/calculator/MainActivity.java
@@ -6,12 +6,12 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private Button TypeStates[] = new Button[3];
     private View ButtonZone;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -24,16 +24,13 @@ public class MainActivity extends AppCompatActivity {
 
             switch (item.getItemId()) {
                 case R.id.navigation_simple:
-                    mTextMessage.setText(R.string.title_simple);
-                    ChangeTargetLayout(ButtonZoneContainer, ButtonZone, R.layout.simple_calc);
+                    ChangeTargetLayout(ButtonZoneContainer, ButtonZone, R.layout.simple_calc, TypeStates, 0);
                     return true;
                 case R.id.navigation_scientific:
-                    mTextMessage.setText(R.string.title_scientific);
-                    ChangeTargetLayout(ButtonZoneContainer, ButtonZone, R.layout.scientific_calc);
+                    ChangeTargetLayout(ButtonZoneContainer, ButtonZone, R.layout.scientific_calc, TypeStates, 1);
                     return true;
                 case R.id.navigation_graphing:
-                    mTextMessage.setText(R.string.title_graphing);
-                    ChangeTargetLayout(ButtonZoneContainer, ButtonZone, R.layout.graphing_calc);
+                    ChangeTargetLayout(ButtonZoneContainer, ButtonZone, R.layout.graphing_calc, TypeStates, 2);
                     return true;
             }
             return false;
@@ -45,19 +42,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize member variables
+        TypeStates[0] = findViewById(R.id.button_type_simple);
+        TypeStates[1] = findViewById(R.id.button_type_scientific);
+        TypeStates[2] = findViewById(R.id.button_type_graphing);
         ViewGroup ButtonZoneContainer = findViewById(R.id.layout_button_zone);
 
-        ChangeTargetLayout(ButtonZoneContainer, ButtonZone, R.layout.simple_calc);
+        // Set initial state to simple calculator
+        ChangeTargetLayout(ButtonZoneContainer, ButtonZone, R.layout.simple_calc, TypeStates, 0);
 
-        mTextMessage = findViewById(R.id.message);
+        
+
+        // This is the old BottomNavigationView code
+        // TODO - Remove the BottomNavigationView
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
-    private void ChangeTargetLayout(ViewGroup TargetViewSpace, View TargetView, int TargetId) {
+    private void ChangeTargetLayout(ViewGroup TargetViewSpace, View TargetView, int TargetId, Button[] TypeStates, int State) {
         if (TargetView != null) {
             TargetViewSpace.removeView(TargetView);
         }
+        TypeStates[0].setTextColor(getResources().getColor(R.color.colorTextLight));
+        TypeStates[1].setTextColor(getResources().getColor(R.color.colorTextLight));
+        TypeStates[2].setTextColor(getResources().getColor(R.color.colorTextLight));
+        TypeStates[State].setTextColor(getResources().getColor(R.color.colorTextSelected));
         TargetView = getLayoutInflater().inflate(TargetId, null);
         TargetViewSpace.addView(TargetView);
     }
