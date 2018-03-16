3947d916c14dbb66e0fdfa3e57a6b158f608b8b5
Derek Blue <derekmblue4011@gmail.com>
2018-03-16 12:54:27 -0300

    Started Calculator Internals

--- app/src/main/java/ca/bds/edu/calculator/MainActivity.java
+++ app/src/main/java/ca/bds/edu/calculator/MainActivity.java
@@ -6,12 +6,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button TypeStates[];// = new Button[3];
-    private int StateIds[];// = new int[3];
+    // Layout Control
+    private Button TypeStates[];
+    private int CurrentState;
+    private int StateIds[];
     private View LayoutContent;
     private ViewGroup LayoutRoot;
+
+    // Simple Calculator Internals
+    private Button SimpleButtons[];
+    private int ButtonStrings[];
+    private double valueOne = Double.NaN;
+    private double valueTwo;
+    private double evalOne, evalTwo, subEvalOne, subEvalTwo, getSubEvalThree, getSubEvalFour;
+    private char CURRENT_OPERATION;
+    DecimalFormat decimalFormat = new DecimalFormat("#.##########");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,25 +33,27 @@ public class MainActivity extends AppCompatActivity {
 
         // Initialize member variables
         TypeStates = new Button[]{findViewById(R.id.button_type_simple), findViewById(R.id.button_type_scientific), findViewById(R.id.button_type_graphing)};
-        StateIds = new int[]{R.layout.simple_calc, R.layout.scientific_calc, R.layout.graphing_calc};
         LayoutRoot = findViewById(R.id.layout_button_zone);
+        StateIds = new int[]{R.layout.simple_calc, R.layout.scientific_calc, R.layout.graphing_calc};
 
         // Set initial state to simple calculator
         ChangeTypeState(0);
 
         // Top nav bar button listeners
-        for (int i = 0; i < 3; i++) {
-            final int test = i;
+        for (int i = 0; i < TypeStates.length; i++) {
+            final int ButtonIndex = i;
             TypeStates[i].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    ChangeTypeState(test);
+                    ChangeTypeState(ButtonIndex);
                 }
             });
         }
+
     }
 
     private void ChangeTypeState(int State) {
+        CurrentState = State;
         // Reset layout content
         if (LayoutContent != null) {
             LayoutRoot.removeView(LayoutContent);
